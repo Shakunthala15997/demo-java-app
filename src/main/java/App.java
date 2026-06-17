eb77af9302742fea6730b74302b4b52339fcf5f8
Shakunthala15997 <160107334+Shakunthala15997@users.noreply.github.com>
2026-06-17 17:31:31 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) {
+        String password = "admin123";   // Hardcoded secret
         String s = null;
-        System.out.println(s.length());   // SonarQube detects bug
+        System.out.println(s.length());
     }
 }
